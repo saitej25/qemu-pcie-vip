f139cc3cdc98dffb2d693a78f71aa5071b7e6211
TJ <sbodanki@memcpu.com>
2026-07-27 20:10:47 +0000

    Define Verilator timestamp callback

--- rtl/alex/verilator_main.cpp
+++ rtl/alex/verilator_main.cpp
@@ -2,6 +2,13 @@
 #include "Valex_verilator_compat_tb.h"
 #include "verilated.h"
 
+// Verilator 4.x emits a reference to this legacy timestamp callback when
+// compiling SystemVerilog that uses $time, even without SystemC enabled.
+double sc_time_stamp()
+{
+    return 0.0;
+}
+
 int main(int argc, char **argv)
 {
     Verilated::commandArgs(argc, argv);
